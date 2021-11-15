463695de4ac9a1a7f886d62f63dd77b1d0e1e179
Galym Parpiyev <parpiyev.galym@protonmail.com>
2021-11-15 18:10:51 +0900

    seeding working, model done

--- app/models/space.rb
+++ app/models/space.rb
@@ -2,10 +2,10 @@ class Space < ApplicationRecord
   belongs_to :user
 
   CATEGORIES = ["restaurant", "cafe", "coffee stand", "gallery", "retail space", "food-truck", "atelier"]
-  validates :category, inclusion: { in: CATEGORY }
+  validates :category, inclusion: { in: CATEGORIES }
   validates :name, presence: true
   validates :description, presence: true
   validates :price, presence: true, numericality: { only_integer: true }
-  validates :address, presence: true, uniquness: true
+  validates :address, presence: true, uniqueness: true
   validates :size, presence: true, numericality: { only_integer: true }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+puts "Cleaning the db..."
+Space.destroy_all
+
+puts 'Creating 10 Spaces...'
+10.times do
+  Space.create!(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.full_address,
+    category: Space::CATEGORIES.sample,
+    description: "Ipsum lorem",
+    size: 100,
+    price: 1000,
+    user: User.first
+
+  )
+end
+puts "...created #{Space.count} spaces"
+
+
+
+puts 'Creating 10 Users...'
+10.times do
+  User.create!(
+    email:Faker::Internet.email,
+    password: "123123"
+  )
+end
+puts "...created #{User.count} users"
+
+
+puts 'Creating 10 Booking...'
+Space.all.each do |space|
+  Booking.create!(
+  start_date: Date.today,
+  end_date: Date.today + 1,
+  status: "booked",
+  user: User.all.sample,
+  space: space
+  )
+end
+puts "...created #{Booking.count} booking"
