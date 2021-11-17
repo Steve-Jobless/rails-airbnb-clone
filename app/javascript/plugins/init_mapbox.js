1117fd58f4ec817204d70af3465add7423e9c581
Galym Parpiyev <parpiyev.galym@protonmail.com>
2021-11-18 00:00:32 +0900

    added popup card on markers

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -21,8 +21,11 @@ const initMapbox = () => {
     };
 
     markers.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+      
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup) // add this
         .addTo(map);
     });
 
